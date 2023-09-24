15c2f96127f79d2ce61b9efedb7df429809ed84c
pthibaud <pthibaud@users.noreply.github.com>
2023-09-24 18:29:51 +0200

    update license

--- Sources/spinswift/core/PeriodicTable.swift
+++ Sources/spinswift/core/PeriodicTable.swift
@@ -1,3 +1,8 @@
+/*
+This work is licensed under the Creative Commons Attribution-ShareAlike 4.0 International License. To view a copy of this licen
+se, visit http://creativecommons.org/licenses/by-sa/4.0/ or send a letter to Creative Commons, PO Box 1866, Mountain View, CA 9
+4042, USA.
+*/
 import Foundation
 
 /// A class for managing periodic table for atomic properties
