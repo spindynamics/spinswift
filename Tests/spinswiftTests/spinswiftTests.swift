7b1bef2f03ec1b3cd28332c2d6c0b7b0e612fd22
pthibaud <pthibaud@users.noreply.github.com>
2024-07-31 13:45:58 +0200

    verification test for CGSL

--- Tests/spinswiftTests/spinswiftTests.swift
+++ Tests/spinswiftTests/spinswiftTests.swift
@@ -1,9 +1,13 @@
 import XCTest
 @testable import spinswift
+@testable import CGSL
 
 final class spinswiftTests: XCTestCase {
     func testPeriodicTable() throws {
         XCTAssertEqual(PeriodicTable().radius("Oxygen",method:"calc"), 48)
         XCTAssertEqual(PeriodicTable().Z_label("Iron"), "Fe")
+    }
+    func testGSL() throws {
+        XCTAssertEqual(gsl_sf_bessel_J0(1.0),0.7651976865579666)
     }
 }
