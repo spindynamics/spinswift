ceeaf5c31e3c0c62d225415da4fc04117e578d7e
pthibaud <pthibaud@users.noreply.github.com>
2024-07-31 11:40:57 +0200

    removing .linux

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package: Package = Package(
     name: "spinswift",
-    platforms: [.macOS(.v10_15),.linux],
+    platforms: [.macOS(.v10_15)],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
