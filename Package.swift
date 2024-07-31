5be3eb6ea0749a18b377290c6676101db9c9b725
pthibaud <pthibaud@users.noreply.github.com>
2024-07-31 11:28:31 +0200

    branch gfm swift-cmark

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package: Package = Package(
     //    .package(url: "https://github.com/apple/swift-docc-plugin", branch: "main"),
     //    .package(url: "https://github.com/apple/swift-format", from: "508.0.1"),
     //    .package(url: "https://github.com/apple/swift-argument-parser", from: "1.3.0"),
-        .package(url:"https://github.com/apple/swift-cmark", branch:"release/5.10.1"),
+        .package(url:"https://github.com/apple/swift-cmark", branch:"gfm"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
